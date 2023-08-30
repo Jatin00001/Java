65d7c3e413eb6ad1ff09f6f53ea989cf058fbb1c
Jatin <datajatin90@gmail.com>
2023-08-30 22:39:21 +0530

    added

--- binaryconversion.java
+++ binaryconversion.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class binaryconversion{
     public static void main(String[] args){
 //        System.out.println("HEllo World! this is jatin side");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Age ?");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter YOur Name : ");
+        String a = sc.nextLine();
+        System.out.println("age  is " +n);
+        System.out.println("Name is "+a);
     }
 }
 
